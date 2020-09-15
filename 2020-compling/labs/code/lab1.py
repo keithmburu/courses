9b07ba5f2cc4bceb0fd99340873643166c2f5fa9
Alvin Grissom II <acgrissom@gmail.com>
2020-09-15 13:31:33 -0400

    lab1 code update

--- 2020-compling/labs/code/lab1.py
+++ 2020-compling/labs/code/lab1.py
@@ -2,18 +2,16 @@ import sys
 from random import choices
 
 def main():
-    print(roll_die())
+        print(roll_die())
 
-"""
+        """
 Flips a possibly-biased coin. 
-Finish this function.
-
 The first argument of choices is the choices;
 the second is the probability distribution of the choices
 """
 def roll_die(probs=[0.5, 0.5]):
     return choices(range(len(probs)), probs)
-    
-    
+
+
 if __name__ == "__main__":
     main()
