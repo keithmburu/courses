ed2a9acbdfabfdc47fdc5a5539ce81fbec8c54ab
Alvin Grissom II <acgrissom@gmail.com>
2022-03-15 22:24:27 -0400

    lab 6

---
+++ 2022-ml/labs/code/lab6.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_gaussian_quantiles
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from collections import defaultdict
+
+
+
+X, y = make_gaussian_quantiles(
+    n_samples=13000, n_features=10, n_classes=3, random_state=1
+)
+
+n_split = 3000
+
+X_train, X_test = X[:n_split], X[n_split:]
+y_train, y_test = y[:n_split], y[n_split:]
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+digits = datasets.load_digits()
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1)) #flatten
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.1, shuffle=True
+)
+
+depths = [1]
+estimators = [x for x in range(1,11) if x % 10 == 0]
+accuracies = []
+classifiers = defaultdict(dict)
+scores = defaultdict(dict)
+for depth in depths:
+    for max_estimators in estimators:
+        classifier = AdaBoostClassifier(
+            DecisionTreeClassifier(max_depth=depth),
+            n_estimators=max_estimators,
+            learning_rate=1.0,
+            algorithm="SAMME")
+        classifier.fit(X_train, y_train)
+        classifiers[depth][max_estimators] = classifier
+        scores[depth][max_estimators] = classifier.score(X_test, y_test)
+
+    
+
+
+print("Num estimators", str(len(classifiers[1][10].estimators_)))
+# Algorithm will stop adding new estimators if training data is fit perfectly,
+# So, max_estimators may not be the actual number of estimators.
+print(scores[1][10])
+
+
+
---
+++ 2022-ml/labs/lab6.md
@@ -0,0 +1,15 @@
+# Lab 6: AdaBoost (10 points)
+
+Conventional wisdom is that AdaBoost needs weak learners to perform well, but work by [Wyner et al. (2017)](https://www.jmlr.org/papers/volume18/15-240/15-240.pdf) argues that this isn't true. 
+
+In this lab, you'll use AdaBoost on MNIST.  You'll use the scikit-learn implementation to examine the effect of decision tree depth and the number of decision trees on accuracy on MNIST.  I've provided some code to get you started.  Can your make it onto the [MNIST leaderboard](https://paperswithcode.com/sota/image-classification-on-mnist)?  
+
+In your analysis, describe the trends you found and how they do or do not comport with known analyses of AdaBoost.  Use citations where necessary (preferably with BibTeX).
+
+Hints:
+
+1. You should, at a minimum try weak learners at least up to the number of features, but there is no need to stop there.
+2. Be careful about confusing the maximum number of base learners with the actual number of base learners. 
+3. Put your results in a LaTeX table or, better, in plots.  
+4. While not required for this lab, the number of training examples may make a difference.  You can use cross-validation.
+
